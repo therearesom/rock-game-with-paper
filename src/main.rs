ca62d475992e3bdaf5add6751faebd6e483c6a0f
therearesom <jernej2468c@gmail.com>
2025-11-11 22:37:18 +0100

    added game

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
-fn computer_output (n: u32) -> u32 {
-    
-   n 
+fn computer_output () -> u32 {
+    let n: u32 = 1;
+    n
 }
 
+fn game (computer: u32, person: u32) -> u32 {
+    let mut math: i32 = 0;
+    let mut outcome: u32 = 3;
+    math = computer as i32 - person as i32;
+    if math == 0 {
+        return 1;
+    }
+    else if math == 2 || if math == -1 {
+        return 0;
+    }
+    else if math == -2 || if math == 1 {
+        return 2;
+    }
+    else {
+        return 3;
+    }
+}
 
 
 
 fn main() {
     let mut outcome: u32 = 3;
+    let mut n: u32 = 1;
     println!("Let's play rock, paper, scissors!");
-    println!("Enter your choice. (rock -> 1, paper -> 2, scissors -> 3)");
+    println!("Enter your choice and press enter. (rock -> 1, paper -> 2, scissors -> 3)");
     
+
+
+
+
+    
+
     if outcome == 0 {
         println!("Computer wins!");
     }

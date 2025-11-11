e5385a90071ea650de70839fd2bef243152d4549
therearesom <jernej2468c@gmail.com>
2025-11-11 15:15:23 +0100

    added if in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+fn computer_output (n: u32) --> u32 {
+    
+}
+
+
+
+
 fn main() {
     println!("Hello, wrld!");
+    
+    if outcome == 0 {
+        println!("Computer wins!");
+    }
+    else if outcome == 1 {
+        println!("Draw!");
+    }
+    else if outcome == 2 {
+        println!("Player wins!");
+    }
 }

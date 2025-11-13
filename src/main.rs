7f6c312cd13c602f551d2eb7c633891779a7b9a8
therearesom <jernej2468c@gmail.com>
2025-11-13 13:18:15 +0100

    GAME WORKS

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-
+use std::io;
 
 fn computer_output () -> u32 {
     let mut rng = rand::thread_rng();
@@ -18,30 +18,24 @@ fn game (computer: u32, person: u32) -> u32 {
     }
 }
 
-
-
 fn main() {
     println!("Let's play rock, paper, scissors!");
+    let mut input = String::new();
     println!("Enter your choice and press enter. (rock -> 1, paper -> 2, scissors -> 3)");
     
-    let person: u32 = 3;    //paper
+    io::stdin().read_line(&mut input).expect("Reading failed");
+
+    let person: u32 = input.trim().parse().expect("Enter a valid number!");
     let computer: u32 = computer_output ();
 
     println!("You played {}.", person);
     println!("Computer played {}.", computer);
     println!("Game output is {}.", game(computer, person));
 
-    if game(computer, person) == 0 {
-        println!("Computer wins!");
+    match game(computer, person) {
+        0 => println!("Computer wins!"),
+        1 => println!("Draw!"),
+        2 => println!("Player wins!"),
+        _ => println!("Enter a valid number."),
     }
-    else if game(computer, person) == 1 {
-        println!("Draw!");
-    }
-    else if game(computer, person) == 2 {
-        println!("Player wins!");
-    }
-    else {
-        println!("Enter a valid number.");
-    }
-
 }
